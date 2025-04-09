be31b8626753f7b2d8d48dce8de435ecae2f82bd
Gary NG <130442521+NkkGary@users.noreply.github.com>
2025-04-09 18:22:42 +0800

    Updates

--- src/components/Contact.js
+++
@@ -1,8 +0,0 @@
-// src/components/Contact.js
-import React from 'react';
-
-function Contact() {
-  return <h2>Contact Page</h2>;
-}
-
-export default Contact;
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-// src/components/index.js
-export { default as Navbar } from './Navbar';
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Contact } from './Contact';
-export { default as Auth } from './Auth';
-export { default as NotFound } from './NotFound';
